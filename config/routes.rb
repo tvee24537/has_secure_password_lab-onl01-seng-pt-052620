ab9272c16df4803bd2d2207e2759b3fdbe5df0f3
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-27 21:15:45 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+
+  root 'welcome#home'
+
 end
